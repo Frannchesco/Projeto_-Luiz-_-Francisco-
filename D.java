5b896a7da8a553eee850625d7a8a7c95ef0ffbb0
Frannchesco <153007646+Frannchesco@users.noreply.github.com>
2025-09-22 20:02:02 -0300

    Criação da classe D

---
+++ D.java
@@ -0,0 +1,13 @@
+public class D {
+    private float D1;
+    private int D2;
+
+    public void setD1(float D1){this.D1 = D1;}
+    public float getD1(){return D1;}
+
+    public void setD2(int D2){this.D2 = D2;}
+    public int getD2(){return D2;}
+
+    public void MD1() { System.out.println("MD1"); }
+    public void MD2() { System.out.println("MD2"); }
+}
